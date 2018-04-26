a3bc5c5f8cf758df5953b72d4862caf3ba58c97e
Carlos Andres Torres <caletorres_59@hotmail.com>
2018-04-26 11:14:16 -0500

    aplicacion redes sociales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,36 @@
-import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
+import { PowerPage } from '../pages/power/power';
+import { HomePage } from '../pages/home/home';
+import { Component, ViewChild } from '@angular/core';
+import { Platform, IonicPage,NavController } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
 import { TabsPage } from '../pages/tabs/tabs';
+import { FriendsPage } from '../pages/friends/friends'
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  @ViewChild('NAV') nav: NavController; 
   rootPage:any = TabsPage;
-
+  
+  //Creo lista con todos los elementos
+  public pages: Array<{titulo: string, component: any, icon: string}>;
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+    this.rootPage = HomePage;
+    this.pages = [
+      { titulo: 'home', component: TabsPage, icon: 'home' },
+      { titulo: 'friends', component: FriendsPage, icon: 'persons' },
+      {titulo: 'power', component:PowerPage, icon: 'power-outline'}]
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+  showPage(pageP)
+  {
+    this.nav.setRoot(pageP);
   }
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,38 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  cont: number;
+  coments: boolean = false;
+  commentsUsers = [
+    { usuario: 'carlos', comentario: 'Lorem ipsum dolor sit amet consectetur, adipisicing' },
+    { usuario: 'juan', comentario: 'Lorem ipsum dolor sit amet consectetur, adipisicing' }, 
+    { usuario: 'andres', comentario: 'Lorem ipsum dolor sit amet consectetur, adipisicing' },
+    { usuario: 'alvaro', comentario: 'Lorem ipsum dolor sit amet consectetur, adipisicing' }
+  
+  
+  
+  ];
   constructor(public navCtrl: NavController) {
-
+    this.cont = 0;
   }
+  //añade comentarios
+  addComment(input: HTMLInputElement)
+  {
+    this.commentsUsers.push({usuario: 'cale',comentario: input.value})
+    input.value = '';
+  }
+  // showAlert() {
+  //   let alert = this.alertCtrl.create({
+  //     title: 'New Friend!',
+  //     subTitle: 'Your friend, Obi wan Kenobi, just accepted your friend request!',
+  //     buttons: ['OK']
+  //   });
+  //   alert.present();
+  // }
 
 }
